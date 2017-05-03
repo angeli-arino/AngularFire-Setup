5e0fff338ab2f30a157f0c9df07b07f3afa08623
Angeli Arino <aba101@uclive.ac.nz>
2017-05-03 17:36:06 +1200

    addContact actually worked

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+
+import { AddContact } from '../add-contact/add-contact';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  contactList: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.contactList = af.database.list('/contacts');
   }
 
+  addContact(){
+    this.navCtrl.push(AddContact);
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
