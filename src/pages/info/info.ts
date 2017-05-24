7f5bbe2ed30030b179793efa2988a7ffbf3ca371
Angeli Arino <aba101@uclive.ac.nz>
2017-05-24 20:37:00 +1200

    added infor page

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,41 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-//Pages
-import { TabsPage } from '../tabs/tabs';
-
-
 /**
- * Generated class for the Choice page.
+ * Generated class for the Info page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
 @IonicPage()
 @Component({
-  selector: 'page-choice',
-  templateUrl: 'choice.html',
+  selector: 'page-info',
+  templateUrl: 'info.html',
 })
-export class Choice {
-
+export class Info {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Choice');
-  }
-
-  chooseThai() {
-
-  this.navCtrl.setRoot(TabsPage);
-
-
-  }
-
-  chooseChina() {
-  this.navCtrl.setRoot(TabsPage);
-
+    console.log('ionViewDidLoad Info');
   }
 
 }
