c9e4d70db7c812d4fa458d28a2b829d5afeb8de2
Angeli Arino <aba101@uclive.ac.nz>
2017-07-31 00:04:21 +1200

    added alerts

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  alertList: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.alertList = af.database.list('/alerts');
   }
 
 }
