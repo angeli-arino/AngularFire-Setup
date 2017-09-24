40a1836226b3d7de4bb107eb7136f2c3f067af79
Angeli Arino <aba101@uclive.ac.nz>
2017-09-24 14:53:47 +1300

    fixed confirm password and added authentication code

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, Platform } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-
 
 import { AddContact } from '../add-contact/add-contact';
 
@@ -13,12 +12,14 @@ declare var window;
 })
 export class ContactPage {
   id;
+  start;
   finalContacts = [];
   contactList: FirebaseListObservable<any>;
   keyContacts: FirebaseListObservable<any>;
 
   constructor(public navCtrl: NavController, public af: AngularFire,
-  public navParams: NavParams) {
+  public navParams: NavParams, public alertController: AlertController, public platform: Platform) {
+  this.start = "";
     this.id = navParams.get('travelID');
     this.contactList = af.database.list('/contacts');
     this.keyContacts = af.database.list('/keyContacts', {
@@ -33,7 +34,52 @@ export class ContactPage {
     this.navCtrl.push(AddContact);
   }
 
-  callNumber(passedNo) {
-    window.location = "tel:" + passedNo
+  map(passedLoc) {
+   if (passedLoc != "") {
+    if (this.platform.is('android')) {
+      window.location.href = "http://maps.google.com/?q="+ passedLoc;
+    }
+    else if (this.platform.is('ios')) {
+      window.location.href = "http://maps.google.com/?q="+ passedLoc;
+    }
+
+     }
+   else {
+     let alert = this.alertController.create();
+     alert.setTitle("Map Failed");
+     alert.setSubTitle("There is no address!");
+     alert.addButton("Dismiss");
+
+     alert.present();
+   }
   }
+
+  email(passedEmail) {
+    if(passedEmail != "") {
+      window.location.assign('mailto:'+passedEmail);
+    }
+    else {
+      let alert = this.alertController.create();
+      alert.setTitle("Email Failed");
+      alert.setSubTitle("There is no email address!");
+      alert.addButton("Dismiss");
+
+      alert.present();
+    }
+  }
+
+  callNumber(passedNo) {
+    if (passedNo !="") {
+    window.location = "tel:" + passedNo
+    }
+    else {
+      let alert = this.alertController.create({
+        title: 'Phone Call Failed',
+        subTitle: 'There is no phone number!',
+        buttons: ['Dismiss']
+      });
+      alert.present();
+    }
+  }
+
 }
